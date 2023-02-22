584a424f874f24f02e9fdc5c90bc18184d6dc08c
YimmyC <jim.cassano1@gmail.com>
2023-02-22 11:26:08 -0600

    psuedo coded

---
+++ index.js
@@ -0,0 +1,8 @@
+// Prompt for User Information
+// Enter the team manager’s name, employee ID, email address, and office number
+// Menu to add engineer/intern/or finish building team
+// Select engineer: Enter the engineer’s name, ID, email, and GitHub username,
+// and I am taken back to the menu
+// Select Intern: enter the intern’s name, ID, email, and school,
+// Select Finish
+// Generate HTML File
