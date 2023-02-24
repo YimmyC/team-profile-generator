571d70d133b93a630efe79213526d2d98bdce611
YimmyC <jim.cassano1@gmail.com>
2023-02-24 09:38:38 -0600

    good start, first prompt working and generating html

--- index.js
+++ index.js
@@ -6,3 +6,58 @@
 // Select Intern: enter the intern’s name, ID, email, and school,
 // Select Finish
 // Generate HTML File
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const generateHTML = ({ managerName, managerId, managerEmail, managerNumber }) => `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Profile Generator</title>
+</head>
+<body>
+<div>${managerName}</div>
+<div>${managerId}</div>
+<div>${managerEmail}</div>
+<div>${managerNumber}</div>
+</body>
+</html>`;
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your manager's name?",
+      name: "managerName",
+    },
+    {
+      type: "input",
+      message: "What is your manager's employee ID?",
+      name: "managerId",
+    },
+    {
+      type: "input",
+      message: "What is your manager's email?",
+      name: "managerEmail",
+    },
+    {
+      type: "input",
+      message: "What is your manager's number?",
+      name: "managerNumber",
+    },
+    {
+      type: "list",
+      message: "Add an Engineer, Intern, or Finish?",
+      name: "mainMenu",
+      choices: ["Engineer", "Intern", "Finish"],
+    },
+  ])
+  .then((answers) => {
+    console.log(answers);
+    const parseHTML = generateHTML(answers);
+    // Write to File
+    fs.writeFile("team.html", parseHTML, (err) => (err ? console.error(err) : console.log("Success!")));
+  });
